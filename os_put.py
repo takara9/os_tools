bec5df9e2086fc06793e864ab52f992f50b605a5
root <root@server1.takara.jp.ibm.com>
2017-05-19 11:06:31 +0900

    update

--- os_put.py
+++ os_put.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# dump コマンドと組み合わせて Linux のOSバックアップ
+#  5GB未満のオブジェクトをアップロードする
 #
+
 import object_storage
 import os
 import sys
@@ -13,20 +14,11 @@ f = open('./credentials.json', 'r')
 cred = json.load(f)
 f.close()
 
-#####
 # コンテナとアップロード時のオブジェクト名
-cnt = ''
-obj = ''
-
-# ローカルファイル
-lfn = ''
+cnt = '' # コンテナ名
+obj = '' # オブジェクト名
+lfn = '' # ローカルファイル
 #####
-
-
-# チャンクサイズ(200MB)に分割して登録するための情報
-tfn = 'chunk'
-csz = 209715200
-seq = 1
 
 #
 # メイン
@@ -35,12 +27,14 @@ if __name__ == '__main__':
     argvs = sys.argv  # 引数リストの取得
     argc = len(argvs) # 個数
 
-    if (argc != 3):   
-        print 'Usage: dump -0u -f - / | os_dump.py Container Object_name' 
+    if (argc != 4):   
+        print 'Usage: os_upload.py Local_file Container Object_name' 
         quit() 
 
-    cnt = argvs[1]
-    obj = argvs[2]
+    lfn = argvs[1]
+    cnt = argvs[2]
+    obj = argvs[3]
+
 
     # 認証取得
     oos = object_storage.get_client(cred['username'], cred['password'], datacenter=cred['data_center'])
@@ -60,35 +54,20 @@ if __name__ == '__main__':
         pass
 
 
-    # マニフェストを登録
-    header = {'Content-Length': '0'}
-    mf = "%s/%s/%s" % (cnt,obj,tfn)
-    header['X-Object-Manifest'] = mf
-    oos[cnt][obj].create(headers=header)
-
-    # チャンクサイズ毎にアップロードします。
-    infile_sent = 0.0
-    progress_pst = 0.0
-    fin = sys.stdin
+    # オブジェクト・ストレージへ書き込み
     try:
-        bytes_read = fin.read(csz)
-        while bytes_read:
-            obj2 = "%s/%s-%04d" % (obj, tfn, seq)
-
-            oos[cnt][obj2].create()
-            oos[cnt][obj2].send(bytes_read)
-            seq = seq + 1
-            infile_sent = infile_sent + len(bytes_read)
-            print "sending...  %3.3f MB" % (infile_sent/1000/1000)
-            bytes_read = fin.read(csz)
-
+        print "uploading... "
+        fin = open(lfn, 'rb')
+        data = fin.read()
+        oos[cnt][obj].send(data)
     except IOError as (errno, strerror):
         print "I/O error({0}): {1}".format(errno, strerror)
     except:
         print "Unexpected error:", sys.exc_info()[0]
         raise
     finally:
-        fin.close()
+        fin.close() 
+        print "Complete"
 
 
 
